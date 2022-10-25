a2bfb1239ededae164bc5b088af8904ba288c0c4
Brian Evans <ebrian101@gmail.com>
2022-10-25 15:06:20 +0100

    Fix bug in format filing description
    This bug was found in production by a user. Some filing descriptions don't have an enum, which means it calls replace on an undefined variable when formatting. To solve this, a check is now done that only tries to format if the description has an enum. Logs a warning if not and doesn't throw error.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- backend-shared/companiesHouseApi/formatFilingDescription.ts
+++ backend-shared/companiesHouseApi/formatFilingDescription.ts
@@ -1,8 +1,15 @@
 import type {FilingHistoryItemResource} from "./getFilingHistoryFromApi.js";
 import filingDescriptions from './filingDescriptions.json' assert {type:'json'}
 import formatString from 'string-template'
+import {sharedLogger} from "../loggers.js";
 
 export function formatFilingDescription(transaction: FilingHistoryItemResource){
   //todo: format dates properly and add currency to end for statement-of-capital
+  if(transaction.description in filingDescriptions)
   return formatString(filingDescriptions[transaction.description], transaction.description_values)
+  else{
+    // for legacy filing descriptions, maybe it needs to return the description value?
+    sharedLogger.warn({transaction},'Filing description not found for '+transaction.description)
+    return transaction.description
+  }
 }
